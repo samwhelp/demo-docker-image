636e9dfa6be8fe781f671e347ef6f4e097ee5736
samwhelp <samwhelp@users.noreply.github.com>
2026-01-14 17:37:47 +0800

    fix

--- demo/ubuntu/debootstrap/demo-debootstrap-in-docker/ubuntu-25.10/box-run-portal.sh
+++ demo/ubuntu/debootstrap/demo-debootstrap-in-docker/ubuntu-25.10/box-run-portal.sh
@@ -4,4 +4,16 @@
 
 
 ## work for debootstrap in docker (--privileged)
-podman run -it --rm --replace --privileged --name ubuntu-iso-factory-25.10 ubuntu-iso-factory-25.10 /opt/src/build.sh
+#podman run -it --rm --replace --privileged --name ubuntu-iso-factory-25.10 ubuntu-iso-factory-25.10 /opt/src/build.sh
+
+
+
+
+##
+## ## create port dir
+##
+
+mkdir -p ./port
+
+## work for debootstrap in docker (--privileged)
+podman run -it  -v ./port:/port --rm --replace --privileged --name ubuntu-iso-factory-25.10 ubuntu-iso-factory-25.10 /opt/src/build.sh
--- demo/ubuntu/debootstrap/demo-debootstrap-in-docker/ubuntu-25.10/box-run.sh
+++ demo/ubuntu/debootstrap/demo-debootstrap-in-docker/ubuntu-25.10/box-run.sh
@@ -8,4 +8,16 @@
 
 
 ## work for debootstrap in docker (--privileged)
-podman run -it --rm --replace --privileged --name ubuntu-iso-factory-25.10 ubuntu-iso-factory-25.10
+#podman run -it --rm --replace --privileged --name ubuntu-iso-factory-25.10 ubuntu-iso-factory-25.10
+
+
+
+
+##
+## ## create port dir
+##
+
+mkdir -p ./port
+
+## work for debootstrap in docker (--privileged)
+podman run -it -v ./port:/port --rm --replace --privileged --name ubuntu-iso-factory-25.10 ubuntu-iso-factory-25.10
