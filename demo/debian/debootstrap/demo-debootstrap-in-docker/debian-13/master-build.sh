3fcf7332e41a27f604a374bfac4c40e4ccfe10cd
samwhelp <samwhelp@users.noreply.github.com>
2026-01-20 18:17:27 +0800

    fix

--- demo/debian/debootstrap/demo-debootstrap-in-docker/debian-13/master-build.sh
+++ demo/debian/debootstrap/demo-debootstrap-in-docker/debian-13/master-build.sh
@@ -19,5 +19,5 @@ rm -rf ./port
 mkdir -p ./port
 
 ## work for debootstrap in docker (--privileged)
-#podman run -it  -v ./port:/port --rm --replace --privileged --name debian-iso-factory-13-run debian-iso-factory-13 /opt/src/build.sh
-podman run -it  -v ./port:/port --replace --privileged --name debian-iso-factory-13-run debian-iso-factory-13 /opt/src/build.sh
+#podman run -it  -v ./port:/port --rm --replace --privileged --name debian-iso-factory-13-run debian-iso-factory-13 /opt/prj/src/build.sh
+podman run -it  -v ./port:/port --replace --privileged --name debian-iso-factory-13-run debian-iso-factory-13 /opt/prj/src/build.sh
--- demo/ubuntu/debootstrap/demo-debootstrap-in-docker/ubuntu-25.10/master-build.sh
+++ demo/ubuntu/debootstrap/demo-debootstrap-in-docker/ubuntu-25.10/master-build.sh
@@ -19,5 +19,5 @@ rm -rf ./port
 mkdir -p ./port
 
 ## work for debootstrap in docker (--privileged)
-#podman run -it  -v ./port:/port --rm --replace --privileged --name ubuntu-iso-factory-25.10-run ubuntu-iso-factory-25.10 /opt/src/build.sh
-podman run -it  -v ./port:/port --replace --privileged --name ubuntu-iso-factory-25.10-run ubuntu-iso-factory-25.10 /opt/src/build.sh
+#podman run -it  -v ./port:/port --rm --replace --privileged --name ubuntu-iso-factory-25.10-run ubuntu-iso-factory-25.10 /opt/prj/src/build.sh
+podman run -it  -v ./port:/port --replace --privileged --name ubuntu-iso-factory-25.10-run ubuntu-iso-factory-25.10 /opt/prj/src/build.sh
