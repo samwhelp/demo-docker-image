3355a28e48fdd8b8135305c91356b6bb2f1b49e9
samwhelp <samwhelp@users.noreply.github.com>
2026-01-20 14:37:21 +0800

    fix

--- demo/ubuntu/debootstrap/demo-debootstrap-in-docker/ubuntu-25.10/src/build.sh
+++ demo/ubuntu/debootstrap/demo-debootstrap-in-docker/ubuntu-25.10/src/build.sh
@@ -4,6 +4,55 @@
 
 
 ##
+## ## Init
+##
+
+REF_CMD_FILE_NAME="$(basename "${0}")"
+REF_BASE_DIR_PATH="$(dirname "$(realpath "${0}")")"
+
+
+
+
+##
+## ## Path / Base
+##
+
+REF_PLAN_DIR_PATH="$(realpath "${REF_BASE_DIR_PATH}/..")"
+REF_BUILD_DIR_PATH="${REF_PLAN_DIR_PATH}/build"
+REF_PORT_DIR_PATH="/port"
+
+
+
+
+##
+## ## Path / Distro
+##
+
+REF_DISTRO_FILE_NAME="distro"
+REF_DISTRO_DIR_PATH="${REF_BUILD_DIR_PATH}/${REF_DISTRO_FILE_NAME}"
+
+REF_DISTRO_IMG_DIR_PATH="${REF_DISTRO_DIR_PATH}/img"
+REF_DISTRO_OUT_DIR_PATH="${REF_DISTRO_DIR_PATH}/out"
+
+REF_DISTRO_FOOTFS_FILE_NAME="rootfs.tar.gz"
+REF_DISTRO_FOOTFS_FILE_PATH="${REF_DISTRO_OUT_DIR_PATH}"
+
+
+
+
+##
+## ## Path / Debootstrap
+##
+
+REF_BUILD_ARCH="amd64"
+REF_BUILD_PACKAGE_INCLUDE="ca-certificates,openssl,console-setup-linux,console-setup,locales,tzdata"
+REF_BUILD_SUITE="questing"
+REF_BUILD_PACKAGE_REPO_URL="http://tw.archive.ubuntu.com/ubuntu/"
+
+
+
+
+##
 ## ## Env
 ##
 
@@ -21,13 +70,11 @@ echo
 
 mod_build_prepare_dir () {
 
-	mkdir -p ../build
+	mkdir -p "${REF_BUILD_DIR_PATH}"
 
-	mkdir -p ../build/img
+	mkdir -p "${REF_DISTRO_IMG_DIR_PATH}"
 
-	mkdir -p ../build/iso
-
-	mkdir -p ../build/out
+	mkdir -p "${REF_DISTRO_OUT_DIR_PATH}"
 
 }
 
@@ -47,8 +94,7 @@ mod_build_prepare () {
 
 }
 
-
-mod_build_create_base_rootfs () {
+mod_build_create_base_rootfs_sample () {
 
 	echo
 	echo "run: debootstrap"
@@ -64,16 +110,61 @@ mod_build_create_base_rootfs () {
 
 }
 
+mod_build_create_base_rootfs_raw () {
+
+	echo
+	echo "run: debootstrap"
+	echo
+
+	sudo debootstrap \
+		--arch=${REF_BUILD_ARCH} \
+		--variant=minbase \
+		--include=${REF_BUILD_PACKAGE_INCLUDE} \
+		${REF_BUILD_SUITE} \
+		${REF_DISTRO_IMG_DIR_PATH} \
+		${REF_BUILD_PACKAGE_REPO_URL}
+
+
+}
+
+
+mod_build_create_base_rootfs () {
+
+	echo
+	echo "run: debootstrap"
+	echo
+
+
+local run_cmd=$(cat << __EOF__
+	debootstrap
+		--arch=${REF_BUILD_ARCH}
+		--variant=minbase
+		--include=${REF_BUILD_PACKAGE_INCLUDE}
+		${REF_BUILD_SUITE}
+		${REF_DISTRO_IMG_DIR_PATH}
+		${REF_BUILD_PACKAGE_REPO_URL}
+__EOF__
+)
+
+
+	echo
+	echo $run_cmd
+	echo
+	$run_cmd
+
+
+}
+
 mod_build_archive_base_rootfs_v001 () {
 
 	echo
 	echo "run: archive-rootfs"
 	echo
 
-	cd ../build/img
+	cd "${REF_DISTRO_IMG_DIR_PATH}"
 
-	#tar --create --auto-compress --file ../out/rootfs.tar.gz ./*
-	tar --create --auto-compress --file ../out/rootfs.tar.gz .
+	#tar --create --auto-compress --file "${REF_DISTRO_FOOTFS_FILE_PATH}" ./*
+	tar --create --auto-compress --file "${REF_DISTRO_FOOTFS_FILE_PATH}" .
 
 	cd "${OLDPWD}"
 
@@ -85,7 +176,7 @@ mod_build_archive_base_rootfs () {
 	echo "run: archive-rootfs"
 	echo
 
-	tar -C ../build/img -cf ../build/out/rootfs.tar.gz .
+	tar -C "${REF_DISTRO_IMG_DIR_PATH}" -cf "${REF_DISTRO_FOOTFS_FILE_PATH}" .
 
 }
 
@@ -95,16 +186,17 @@ mod_build_cp_rootfs_archive_to_port_dir () {
 	echo "run: copy-to-port"
 	echo
 
-	sudo mkdir -p /port
+	sudo mkdir -p "${REF_PORT_DIR_PATH}"
 
-	if ! [ -f ../build/out/rootfs.tar.gz ]; then
+	if ! [ -f "${REF_DISTRO_FOOTFS_FILE_PATH}" ]; then
 		echo
-		echo "File Not Exist: ../build/out/rootfs.tar.gz"
+		echo "File Not Exist: ${REF_DISTRO_FOOTFS_FILE_PATH}"
 		echo
 		return 0
 	fi
 
-	cp -rfT ../build/out/rootfs.tar.gz /port/rootfs.tar.gz
+
+	cp -f "${REF_DISTRO_FOOTFS_FILE_PATH}" "${REF_PORT_DIR_PATH}/${REF_DISTRO_FOOTFS_FILE_NAME}"
 
 
 	echo
